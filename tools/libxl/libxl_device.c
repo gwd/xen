53b97d8905dcafe9e21d19c8181f58fd9d9a58bf
George Dunlap <george.dunlap@eu.citrix.com>
2015-04-23 15:06:13 +0100

    Revert "libxl: prefer qdisk over blktap when choosing disk backend"
    This reverts commit 0166217103e18368424fbd5ffff01c1ea50d0b17.
    
    There are some disk formats commonly supported by both qdisk and blktap.
    As qdisk is better supported and blktap is unmaintained, we choose qdisk
    over blktap whenever possible.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/libxl/libxl_device.c
+++ tools/libxl/libxl_device.c
@@ -278,8 +278,8 @@ int libxl__device_disk_set_backend(libxl__gc *gc, libxl_device_disk *disk) {
     } else {
         ok=
             disk_try_backend(&a, LIBXL_DISK_BACKEND_PHY) ?:
-            disk_try_backend(&a, LIBXL_DISK_BACKEND_QDISK) ?:
-            disk_try_backend(&a, LIBXL_DISK_BACKEND_TAP);
+            disk_try_backend(&a, LIBXL_DISK_BACKEND_TAP) ?:
+            disk_try_backend(&a, LIBXL_DISK_BACKEND_QDISK);
         if (ok)
             LOG(DEBUG, "Disk vdev=%s, using backend %s",
                        disk->vdev,
